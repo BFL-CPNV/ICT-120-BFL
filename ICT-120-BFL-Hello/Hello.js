81d8ee057a9a84b8edc54e3613525529b1465872
BFL-CPNV <48351345+BFL-CPNV@users.noreply.github.com>
2019-10-03 08:40:19 +0200

    Update of what was stored in my C:

---
+++ ICT-120-BFL-Hello/Hello.js
@@ -0,0 +1,18 @@
+function exercise1_1_js (alert)
+{
+    if (exercise1_1.click)
+    {
+        alert (alert);
+    }
+}
+
+function exercise1_4_js ()
+{
+    var age;
+    age = prompt ();
+
+    if (exercise1_4.click)
+    {
+        alert (age);
+    }
+}
